60ba7026943762f56071fe643c027c755d77054b
Stephen Brewer <stephebrewer16@gmail.com>
2019-06-24 13:06:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+var multiply = num1 * num2
+
+var random = Math.ceil(Math.random() * 10)
+
+var mod = 20 % 16
+
+var max = Math.max(1, 2, 4, 20)
