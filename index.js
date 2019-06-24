b2b5b7cd835c6de68ba9cd258f1c7a32494010fc
chrisyuaners <chrisyuaners@gmail.com>
2019-06-24 15:02:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10 + 1)
+
+let mod = 4 % 10
+
+let max = Math.max(1,20)
